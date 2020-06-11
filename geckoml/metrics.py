184ef074f0923368f069d5a99befc8d91741be55
Charlie Becker <31113893+charlie-becker@users.noreply.github.com>
2020-06-11 09:28:25 -0600

    Add metrics.py for evaluation

---
+++ geckoml/metrics.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import confusion_matrix, mean_squared_error
+import numpy as np
+
+def calc_pdf_hist(x, x_bins):
+    return np.histogram(x, x_bins, density=True)[0]
+
+
+def hellinger(x, pdf_p, pdf_q):
+    pdf_distances = (np.sqrt(pdf_p) - np.sqrt(pdf_q)) ** 2
+    return np.trapz(pdf_distances, x) / 2
+
+
+def root_mean_squared_error(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+def hellinger_distance(y_true, y_pred, bins=50):
+    bin_points = np.linspace(np.minimum(y_true.min(), y_pred.min()),
+                       np.maximum(y_true.max(), y_pred.max()),
+                       bins)
+    bin_centers = 0.5 * (bin_points[:-1] + bin_points[1:])
+    y_true_pdf = calc_pdf_hist(y_true, bin_points)
+    y_pred_pdf = calc_pdf_hist(y_pred, bin_points)
+    return hellinger(bin_centers, y_true_pdf, y_pred_pdf)
