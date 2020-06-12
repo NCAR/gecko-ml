25fd36259f7be3b3eabe9740577291b34144f4c8
Charlie Becker <31113893+charlie-becker@users.noreply.github.com>
2020-06-12 12:15:50 -0600

    Updates per PR review

--- train_gecko_emulators.py
+++ train_gecko_emulators.py
@@ -1,13 +1,21 @@
 from geckoml.models import DenseNeuralNetwork
-from geckoml.data import combine_data, split_data, repopulate_scaler
+from geckoml.data import combine_data, split_data
+from sklearn.preprocessing import StandardScaler, RobustScaler, MaxAbsScaler, MinMaxScaler
+from tensorflow.keras.models import Model, save_model
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
+import joblib
 import numpy as np
 import argparse
 import yaml
 
 start = time.time()
+
+scalers = {"MinMaxScaler": MinMaxScaler,
+           "MaxAbsScaler": MaxAbsScaler,
+           "StandardScaler": StandardScaler,
+           "RobustScaler": RobustScaler}
 
 def main():
     
@@ -28,9 +36,9 @@ def main():
     max_exp = config['max_exp']
     input_vars = config['input_vars']
     output_vars = config['output_vars']
-    save_fortran = config['save_fortran']
+    save_models = config['save_models']
     output_path = config['output_path']
-    
+    scaler_type = config['scaler_type']
 
     # Load GECKO experiment data, split into ML inputs and outputs and persistence outputs
     input_data, output_data = combine_data(dir_path, summary_file, aggregate_bins, bin_prefix,
@@ -40,21 +48,31 @@ def main():
     in_train, out_train, in_val, out_val, in_test, out_test = split_data(input_data, output_data)
 
     # Rescale training and validation / testing data
-    scaled_in_train = repopulate_scaler(in_train).fit_transform(in_train.iloc[:,:-1])
-    scaled_in_val = repopulate_scaler(in_val).fit_transform(in_val.iloc[:,:-1])
-    scaled_out_train = repopulate_scaler(out_train).fit_transform(out_train.iloc[:,:-1])
-    scaled_out_val = repopulate_scaler(out_val).fit_transform(out_val.iloc[:,:-1])
-    
+    X_scaler, Y_scaler = scalers[scaler_type](), scalers[scaler_type]()
+
+    scaled_in_train = X_scaler.fit_transform(in_train.iloc[:,:-1])
+    scaled_in_val = X_scaler.transform(in_val.iloc[:,:-1])
+    scaled_out_train = Y_scaler.fit_transform(out_train.iloc[:,:-1])
+    scaled_out_val = Y_scaler.transform(out_val.iloc[:,:-1])
+
     # Train ML models
-    dnn = DenseNeuralNetwork(hidden_layers=2, hidden_neurons=100,lr=0.0005,batch_size=512, epochs=2)
-    dnn.fit(scaled_in_train,scaled_out_train)
+    models = {}
+    for model_name, model_config in config["model_configurations"].items():
+
+        models[model_name] = DenseNeuralNetwork(**model_config)
+        models[model_name].fit(scaled_in_train, scaled_out_train)
 
     # Calculate validation and testing scores
-    preds = pd.DataFrame(dnn.predict(scaled_in_val).reshape(-1,scaled_out_train.shape[1]))
     
     # Save ML models, scaling values, and verification data to disk
-    if save_fortran:
-        dnn.save_fortran_model(output_path)
+    if save_models:
+
+        for model_name in config["model_configurations"].keys():
+            models[model_name].save_fortran_model(output_path + model_name + ".nc")
+            save_model(models[model_name].model, output_path + model_name + ".h5")
+            
+        joblib.dump(X_scaler, '{}{}_X.scaler'.format(output_path, species))
+        joblib.dump(Y_scaler, '{}{}_Y.scaler'.format(output_path, species))
     
     print('Completed in {0:0.1f} seconds'.format(time.time() - start))
     
