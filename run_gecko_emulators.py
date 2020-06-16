08dc09716a60b7e33c0eb89469c0d02fd012e844
Charlie Becker <31113893+charlie-becker@users.noreply.github.com>
2020-06-16 12:05:36 -0600

    Py script to run box model
    This is a single run, but should be trivial to parallelize. Uses same config file as train_gecko_emulators.py. Parameter options are experiment number, starting timestep (default=0), and total timestep to be ran forward.
    
    Tested okay - Evaluation needs to be added.

--- run_gecko_emulators.py
+++ run_gecko_emulators.py
@@ -1,17 +1,49 @@
 import numpy as np
 import argparse
 import yaml
+import time
+from geckoml.box import GeckoBoxEmulator
+from geckoml.data import get_starting_conds
 
+start = time.time()
 
 def main():
+    
+    # read YAML config as provided arg
     parser = argparse.ArgumentParser()
-    parser.add_argument("config", required=True, help="Path to config file")
+    parser.add_argument("-c", "--config", default="agg_config.yml", help="Path to config file")
     args = parser.parse_args()
     with open(args.config) as config_file:
         config = yaml.load(config_file)
-    # We will want to run multiple GECKO experiments in parallel
-    return
+    
+    # Extract config arguments and validate if necessary
+    species = config['species']
+    dir_path = config['dir_path']
+    summary_file = config['summary_file']
+    aggregate_bins = config['aggregate_bins']
+    bin_prefix = config['bin_prefix']
+    input_vars = config['input_vars']
+    output_vars = config['output_vars']
+    output_path = config['output_path']
 
+    exp_num = 299
+    start_timestep = 0
+    total_timesteps = 1439
+    
+    # single experiment example 
+    # We will want to run multiple GECKO experiments in parallel
+    
+    starting_conditions = get_starting_conds(dir_path, summary_file, bin_prefix, input_vars, output_vars,
+                           aggregate_bins, species, exp_num, start_timestep)
+
+    mod = GeckoBoxEmulator()
+    box_preds = mod.predict(starting_conditions, total_timesteps)
+    
+    print('Completed in {0:0.1f} seconds'.format(time.time() - start))
+    
+    # Add Verification/Analysis and output
+    
+    return
 
 if __name__ == "__main__":
     main()
