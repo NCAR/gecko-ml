b703b55fec923a6bbf98be4d563137ac30867d7a
Charlie Becker <charles11becker@gmail.com>
2020-07-02 18:41:17 -0600

    Begin modularization of Box Class.

--- geckoml/box.py
+++ geckoml/box.py
@@ -13,6 +13,24 @@ class GeckoBoxEmulator(object):
 
         return
 
+    def run_ensemble(self, data, num_exps, num_exps='all'):
+        """ if num_exps == 'all' then... """
+        exps = data['id'].unique()
+
+        if num_exps != 'all':
+
+            exps = np.random.choice(exps, num_exps, replace=False)
+
+        for x in exps:
+
+            sc = self.get_starting_conds(data, x)
+            num_timesteps = len(data)
+            pred = self.predict(sc, num_timesteps)
+
+            # append preds/results of each experimetn
+
+        return result_df
+
     def predict(self, input_concentrations, num_timesteps, starting_ts=0, seq_length=1):
 
         input_scaler = joblib.load(self.input_scaler_path)
@@ -39,3 +57,19 @@ class GeckoBoxEmulator(object):
         results = output_scaler.inverse_transform(pred_array)
 
         return results
+
+    def get_starting_conds(self, data, exp, seq_len=1, starting_ts=0):
+        starting_conditions = data[data['id'] == exp].iloc[starting_ts:starting_ts + seq_len, :]
+        return starting_conditions
+
+    def scale_input(self, input_concentrations):
+
+        input_scaler = joblib.load(self.input_scaler_path)
+        scaled_input = input_scaler.transform(input_concentrations.iloc[starting_ts:seq_length,1:-1])
+        static_input = scaled_input[:, -6:]
+
+        return scaled_input, static_input
+
+    def transform_output(self, predictions):
+        return
+
