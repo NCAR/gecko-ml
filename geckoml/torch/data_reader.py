6b632b1fcd9de60ea6033db97c4c560fac6c3ce4
John Schreck <jsschreck@gmail.com>
2020-11-11 10:17:12 -0700

    Updated the data generator for torch models

--- geckoml/torch/data_reader.py
+++ geckoml/torch/data_reader.py
@@ -12,7 +12,6 @@ import tqdm
 import re
 import os
 
-from multiprocessing import Pool
 
 from sklearn.preprocessing import (StandardScaler, RobustScaler, 
                                    MaxAbsScaler, MinMaxScaler, QuantileTransformer)
@@ -20,6 +19,8 @@ from sklearn.model_selection import GroupShuffleSplit, train_test_split
 from sklearn.pipeline import Pipeline
 from os.path import join, exists
 
+from multiprocessing import Pool
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
@@ -30,40 +31,42 @@ class LoadGeckoData:
                  data_path,
                  save_path,
                  summary_file,  
-                 bin_prefix = [], 
-                 input_vars = [],
-                 output_vars = [],
-                 seq_length = 1, 
+                 bin_prefix = (), 
+                 input_vars = (),
+                 output_vars = (),
                  num_timesteps = 1439,
-                 experiment_subset = [],
-                 cached_dir = "./",
+                 experiment_subset = (),
+                 min_exp = None,
+                 max_exp = None,
+                 cached_dir = None,
+                 memory_buffer = None,
                  shuffle = True,
                  scaler_x = None,
-                 scaler_y = None, 
-                 fit = False, 
-                 *args, **kwargs):
+                 scaler_y = None,
+                 log_precursor = False):
         
         self.path = data_path
         self.save_path = save_path
         self.summary_file = summary_file
         self.experiment_subset = experiment_subset
+        self.min_exp = min_exp
+        self.max_exp = max_exp
         self.cached_dir = cached_dir
+        self.memory_buffer = memory_buffer
         self.bin_prefix = bin_prefix
         self.input_vars = input_vars
         self.output_vars = output_vars
-        self.seq_length = 1
         self.num_timesteps = num_timesteps
         
         self.shuffle = shuffle
         self.scaler_x = scaler_x
         self.scaler_y = scaler_y
-        self.fit = fit
+        self.log_precursor = log_precursor
         
         self.load()
         self.on_epoch_end()
         
-        self.reshape = True
-        if any([self.scaler_x is None, self.scaler_y is None]) or self.fit:
+        if any([self.scaler_x is None, self.scaler_y is None]):
             self.fit_scalers()
             
     def load(self):
@@ -73,7 +76,18 @@ class LoadGeckoData:
             os.path.join(self.path, self.summary_file), skiprows = 3
         )
         self.summary_file.columns = [x.strip() for x in self.summary_file.columns]
-    
+        
+        # Set up a memory buffer if told to
+        if self.memory_buffer is not None:
+            self.memory_buffer = {}
+        
+        # Check if a cache dir was given
+        if self.cached_dir is not None:
+            if not os.path.isdir(self.cached_dir):
+                raise OSError(
+                    "The cached directory was specified but does not exist. Mkdir and try again."
+                )
+
     def get_transform(self):
         return self.scaler_x, self.scaler_y
 
@@ -81,28 +95,44 @@ class LoadGeckoData:
         'Denotes the number of batches per epoch'
         return len(self.experiment_subset)
     
-    def __getitem__(self, idx):
+    def __getitem__(self, idx, fit = False):
         'Generate one data point'
         
         ### Find the relevant experiment file 
         exp = self.experiment_subset[idx]
         
-        ### If we have this experiment already cached, load it
-        cached = f"{self.cached_dir}/{exp}.pkl"
-        if os.path.isfile(cached) and self.scaler_x is not None:
-            with open(cached, "rb") as fid:
-                return pickle.load(fid)
+        ### Do the shuffle, if we asked for that after each epoch,  here
+        ### to ensure that it gets carried out
         
-        ### Else we have to create it for the first time.
+        self.processed += 1
+        if self.processed == self.__len__():
+            self.on_epoch_end()
+        
+        ### First see if we have it in memory
+        if isinstance(self.memory_buffer, dict) and not fit:
+            if idx in self.memory_buffer:
+                return self.memory_buffer[idx]
+            
+        ### If experiment not in memory, check if saving pre-processed data to file
+        if os.path.isdir(self.cached_dir) and not fit:
+            ### If we have this experiment already cached, load it
+            cached = f"{self.cached_dir}/{exp}.pkl"
+            if os.path.isfile(cached) and self.scaler_x is not None:
+                with open(cached, "rb") as fid:
+                    return pickle.load(fid)
+        
+        ### Otherwise we have to create it for the first time.
+        
+        ### First find the relevant file for the selected experiment
         for file_path in self.file_list:
             if f"Exp{exp}" in file_path:
                 break
 
-        ### Load the experiment file
+        ### Next load the experiment file
         df = pd.read_csv(file_path)
         df.columns = [x.strip() for x in df.columns]
 
-        ### Load the summary file
+        ### Then load the summary file
         exp_num = int(re.findall("_Exp(\d+)*", file_path)[0])
         summary_file = self.summary_file[(
             self.summary_file['id'] == f"Exp{exp_num}"
@@ -115,41 +145,55 @@ class LoadGeckoData:
             for prefix in self.bin_prefix:
                 df[prefix] = df.loc[:, df.columns.str.contains(prefix, regex=False)].sum(axis=1)
 
+        ### Add the diurnal signal
+        df = self.add_diurnal_signal(df)        
+                
+        ### Make the input/output arrays
         input_subset = df[self.input_vars].iloc[:-1,:].copy()
         output_subset = df[self.output_vars].iloc[1:,:].copy()
-
+        
+        ### Log the precursor [optional]
+        if self.log_precursor:
+            input_subset['Precursor [ug/m3]'] = np.log(input_subset['Precursor [ug/m3]'])
+            output_subset['Precursor [ug/m3]'] = np.log(output_subset['Precursor [ug/m3]'])
+        
         if "index" in input_subset.columns:
             input_subset = input_subset.drop(columns = ["index"])
         if "index" in output_subset.columns:
             output_subset = output_subset.drop(columns = ["index"])
 
-        input_subset = self.add_diurnal_signal(input_subset)
-
-        self.processed += 1
-        if self.processed == self.__len__():
-            self.on_epoch_end()
-
-        if self.scaler_x is not None:
+        if self.scaler_x is not None: # Gets skipped when fitting the scalers
             input_subset = self.scaler_x.transform(
                 input_subset.drop(['Time [s]', 'id'], axis=1)
             )
-        if self.scaler_y is not None:
+        if self.scaler_y is not None: # Gets skipped when fitting the scalers
             output_subset = self.scaler_y.transform(
                 output_subset.drop(['Time [s]', 'id'], axis=1)
             )
-        if self.reshape:
-            input_subset, output_subset = self.reshape_data(input_subset, output_subset)
+        
+        reshaped_input = torch.from_numpy(input_subset.astype(np.float32))
+        reshaped_output = torch.from_numpy(output_subset.astype(np.float32))
             
+        # Serialize the data and save
+        if os.path.isdir(self.cached_dir) and not fit:
+            cached = f"{self.cached_dir}/{exp}.pkl"
             with open(cached, "wb") as fid:
-                pickle.dump([input_subset, output_subset], fid)
-
-        return input_subset, output_subset
+                pickle.dump((reshaped_input, reshaped_output), fid)
+                
+        # Save the data to the memory buffer
+        if isinstance(self.memory_buffer, dict) and not fit:
+            self.memory_buffer[idx] = (reshaped_input, reshaped_output)
+                
+        if fit:
+            return input_subset, output_subset
+        
+        return reshaped_input, reshaped_output
     
     def on_epoch_end(self):
         'Updates indexes after each epoch'
         self.processed = 0
         if self.shuffle == True:
-            random.shuffle(self.file_list)
+            random.shuffle(self.experiment_subset)
             
     def add_diurnal_signal(self, x_data):
         """
@@ -183,17 +227,21 @@ class LoadGeckoData:
 
         filepath = os.path.join(self.save_path, "scalers.pkl")
         
-        if os.path.isfile(filepath) and not self.fit:
-            #logging.info(f"Loading data preprocessing models from {filepath}")
+        if os.path.isfile(filepath):
+            logger.info(
+                f"Loading fitted data transformation models from {filepath}"
+            )
             with open(filepath, "rb") as fid:
                 self.num_timesteps, self.scaler_x, self.scaler_y = pickle.load(fid)
             
         else:
-            logger.info("Fitting data preprocessing models: QuantileTransformer")
-            self.reshape = False                    
+            logger.info(
+                "Fitting data transformation models: MinMaxScaler((0, 1))"
+            )
+            worker = partial(self.__getitem__, fit = True)
             with Pool(8) as p:
-                xs, ys = zip(*[(x,y) for (x,y) in tqdm.tqdm(
-                    p.imap(self.__getitem__, range(len(self.experiment_subset))),
+                xs, ys = zip(*[(x,y) for (x,y) in tqdm(
+                    p.imap(worker, range(len(self.experiment_subset))),
                     total = len(self.experiment_subset))
                 ])
             p.join()
@@ -202,15 +250,15 @@ class LoadGeckoData:
             xs = pd.concat(xs)
             ys = pd.concat(ys)
             self.reshape = True
-            
+                        
             self.num_timesteps = xs['Time [s]'].nunique()
 
-            self.scaler_x = Pipeline(
-                steps=[('quant', QuantileTransformer()), ('minmax', MinMaxScaler((0, 1)))]
-            )
-            self.scaler_y = Pipeline(
-                steps=[('quant', QuantileTransformer()), ('minmax', MinMaxScaler((0, 1)))]
-            )
+            self.scaler_x = MinMaxScaler((0, 1)) #Pipeline(
+                #steps=[('quant', QuantileTransformer()), ('minmax', MinMaxScaler((0, 1)))]
+            #)
+            self.scaler_y = MinMaxScaler((0, 1)) #Pipeline(
+                #steps=[('quant', QuantileTransformer()), ('minmax', MinMaxScaler((0, 1)))]
+            #)
             
             scaled_in_train = self.scaler_x.fit_transform(
                 xs.drop(['Time [s]', 'id'], axis=1)
@@ -222,25 +270,6 @@ class LoadGeckoData:
             with open(filepath, "wb") as fid:
                 pickle.dump([self.num_timesteps, self.scaler_x, self.scaler_y], fid)
             
-            logger.info(f"Saved data preprocessing models to {filepath}")
-            
-    def reshape_data(self, x_data, y_data):
-        """
-        Reshape matrix data into sample shape for LSTM training.
-
-        Args:
-            x_data: DataFrame containing input features (columns) and time steps (rows).
-            y_data: Matrix containing output features (columns) and time steps (rows).
-            seq_length: Length of look back time steps for one time step of prediction.
-            num_timesteps: Number of time_steps per experiment.
-
-        Returns: Two np.ndarrays, the first of shape (samples, length of sequence,
-            number of features), containing the input data for the LSTM. The second
-            of shape (samples, number of output features) containing the expected output for each input
-            sample.
-        """
-
-        x_data = torch.from_numpy(x_data.astype(np.float32))
-        y_data = torch.from_numpy(y_data.astype(np.float32))
-                
-        return x_data, y_data    
+            logger.info(
+                f"Saved data transformation models to {filepath}"
+            )
