dea346947b806647ec41715b07c5954b63c50692
keelylawrence <57740376+keelylawrence@users.noreply.github.com>
2020-11-11 17:45:17 -0700

    Update models_test.py
    Tests consolidated, added descriptions

--- geckoml/models_test.py
+++ geckoml/models_test.py
@@ -5,38 +5,25 @@ import pandas as pd
 
 np.random.seed(9)
 
+#test run, test shape, test prediction replication
 def test_denseneuralnetwork():
-    nn = DenseNeuralNetwork()
-    x = np.random.random(size=(100, 3))
-    nn.build_neural_network(3,5)
-    y = nn.predict(x)
-    assert not np.isnan(y).any()   
- 
-
-def test_nnshape():
-    nn = DenseNeuralNetwork()
-    x = np.random.random(size=(100, 3))
-    nn.build_neural_network(3,5)
-    y = nn.predict(x)
-    assert y.shape[-1] == 5
-   
-
-def test_loaddata():
-    df = pd.read_csv("/glade/p/cisl/aiml/gecko/ML2019_dodecane_postproc/ML2019_dodecane_ML2019_Exp54.csv", skiprows = 3) 
-    assert not df.isnull().values.any()
-    assert not df.isna().values.any()
-    assert df.shape[0] == 1437
-    assert df.shape[1] == 128
- 
-
-def test_replicateprediction():
     nn = DenseNeuralNetwork()
     x = np.random.random(size=(100, 3))
     nn.build_neural_network(3,5)
     y = nn.predict(x)
     z = nn.predict(x)
     assert np.array_equal(y,z)
-    
+    assert not np.isnan(y).any()
+    assert y.shape[-1] == 5
+
+#test load data  
+def test_loaddata():
+    df = pd.read_csv("/glade/p/cisl/aiml/gecko/ML2019_dodecane_postproc/ML2019_dodecane_ML2019_Exp54.csv", skiprows = 3) 
+    assert not df.isnull().values.any()
+    assert not df.isna().values.any()
+    assert df.shape[0] == 1437
+    assert df.shape[1] == 128
+   
     
 if __name__ == "__main__":
     test_denseneuralnetwork()
