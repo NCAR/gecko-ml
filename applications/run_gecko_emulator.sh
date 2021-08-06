ee8e30f69348daeda6a785680e0fe67f537da3c4
Charlie Becker <charles11becker@gmail.com>
2021-08-06 15:47:32 -0600

    Minor submit script changes

--- applications/run_gecko_emulator.sh
+++ applications/run_gecko_emulator.sh
@@ -3,7 +3,7 @@
 #PBS -A NAML0001
 #PBS -l walltime=00:30:00
 #PBS -q casper
-#PBS -l select=1:ncpus=16:mpiprocs=16:mem=128GB
+#PBS -l select=1:ncpus=8:mem=64GB
 #PBS -o ./run.txt
 #PBS -e ./run.txt
 module load cuda/11 cudnn
--- applications/train_gecko_emulator.sh
+++ applications/train_gecko_emulator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #PBS -N gecko-ml
 #PBS -A NAML0001
-#PBS -l walltime=01:30:00
+#PBS -l walltime=00:30:00
 #PBS -q casper
-#PBS -l select=1:ncpus=16:ngpus=1:mem=128GB -l gpu_type=v100
+#PBS -l select=1:ncpus=8:ngpus=1:mem=64GB -l gpu_type=v100
 #PBS -o ./train.txt
 #PBS -e ./train.txt
 module load cuda/11 cudnn
