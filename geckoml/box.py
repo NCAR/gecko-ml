6b9f0a7368e82f217c1acca36a4f7b4af66968cc
Charlie Becker <charles11becker@gmail.com>
2021-08-06 10:59:34 -0600

    Changes to box model to accomodate tendencies; more flexibility.

--- geckoml/box.py
+++ geckoml/box.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
-import gc
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.python.framework.ops import disable_eager_execution
-from .data import inverse_log_transform
 
 disable_eager_execution()
 
@@ -24,35 +21,36 @@ class GeckoBoxEmulator(object):
           seed (int): Random seed
     """
 
-    def __init__(self, neural_net_path, input_scaler, output_scaler, input_cols, output_cols):
+    def __init__(self, neural_net_path, input_cols, output_cols):
 
         self.neural_net_path = neural_net_path
-        self.input_scaler = input_scaler
-        self.output_scaler = output_scaler
         self.input_cols = input_cols
         self.output_cols = output_cols
 
         return
 
-    def run_box_simulation(self, val_data, exps='all'):
+    def run_box_simulation(self, raw_val_output, transformed_val_input, exps='all'):
         """
         Run an ensemble of GECKO-A experiment emulations distributed over a cluster using dask distributed.
         Args:
-            model: Keras model object
-            val_data (pd.DataFrame): Input Validation/testing dataframe, split by experiment.
+            raw_val_output( pd.DataFrame): Raw validation output dataframe
+            transformed_val_input (pd.DataFrame): Transformed/scaled validation dataframe, split by experiment.
             exps (list of experiments or 'all'):
         Returns:
             results_df (DataFrame): A concatenated pandas DataFrame of emulation results.
         """
+        truth = raw_val_output.copy()
         if exps != 'all':
-            data_sub = val_data.loc[val_data.index.isin(exps, level='id')]
+            data_sub = transformed_val_input.loc[transformed_val_input.index.isin(exps, level='id')]
+            truth = truth.loc[truth.index.isin(exps, level='id')]
         else:
-            data_sub = val_data.copy(deep=True)
+            data_sub = transformed_val_input.copy(deep=True)
+
         n_exps = len(data_sub.index.unique(level='id'))
         n_timesteps = len(data_sub.index.unique(level='Time [s]'))
         n_features = len(self.input_cols)
         out_col_idx = data_sub.columns.get_indexer(self.output_cols)
-        batched_array = self.input_scaler.transform(data_sub).reshape(n_exps, n_timesteps, n_features)
+        batched_array = data_sub.values.reshape(n_exps, n_timesteps, n_features)
         init_array = batched_array[:, 0, :]
         pred_array = np.empty((n_exps, n_timesteps, len(self.output_cols)))
         mod = load_model(self.neural_net_path)
@@ -65,8 +63,8 @@ class GeckoBoxEmulator(object):
                 pred = np.block(mod.predict(new_input))
             new_input = batched_array[:, time_step, :]
             new_input[:, out_col_idx] = pred
-            pred_array[:, time_step, :] = self.output_scaler.inverse_transform(pred)
-        preds_df = pd.DataFrame(data=pred_array.reshape(-1, len(self.output_cols)),
-                                columns=val_data.columns[out_col_idx], index=val_data.index)
-        return data_sub, preds_df
+            pred_array[:, time_step, :] = pred
 
+        preds_df = pd.DataFrame(data=pred_array.reshape(-1, len(self.output_cols)),
+                                columns=raw_val_output.columns, index=raw_val_output.index)
+        return truth, preds_df
