af1fc6922dd3ec65879706c6a8af883dc90de3f5
Charlie Becker <charles11becker@gmail.com>
2021-10-18 12:30:27 -0600

    Minimal pytest changes

--- geckoml/models_test.py
+++ geckoml/models_test.py
@@ -2,23 +2,22 @@ from geckoml.models import DenseNeuralNetwork
 import numpy as np
 import pandas as pd
 
-
 np.random.seed(9)
 
-#test run, test shape, test prediction replication
 def test_denseneuralnetwork():
     nn = DenseNeuralNetwork()
-    x = np.random.random(size=(100, 3))
-    nn.build_neural_network(3,5)
-    y = nn.predict(x)
-    z = nn.predict(x)
-    assert np.array_equal(y,z)
+    x = np.random.random(size=(100, 9))
+    y = np.random.random(size=(100, 3))
+    nn.fit(x, y)
+    p = nn.predict(x)
+    p2 = nn.predict(x)
+    assert np.array_equal(p, p2)
     assert not np.isnan(y).any()
-    assert y.shape[-1] == 5
+    assert y.shape[-1] == 3
 
 #test load data  
 def test_loaddata():
-    df = pd.read_csv("test_data/test_data.csv", skiprows = 3)
+    df = pd.read_csv("./geckoml/test_data.csv", skiprows=3)
     assert not df.isnull().values.any()
     assert not df.isna().values.any()
     assert df.shape[0] == 1437
